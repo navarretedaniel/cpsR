0f0a850390ff77eeb1feb5b9165825a7997d4dc1
Daniel <navarrete.daniel@dol.gov>
2021-02-01 22:09:46 -0500

    Remove library reference.

--- scripts/cps_process.R
+++ scripts/cps_process.R
@@ -11,14 +11,13 @@
 #    cps_process(2018)
 
 # Function ---------------------------------------------------------------------
-library(tidyverse)
 
 cps_process <- function(year){
   input_year <<- as.character(substr(year, 3, 4))
   files <- as.vector(list.files(pattern = "\\.dat$"))
-  
+
   input <- paste0(input_year, "pub.dat$")
-  
+
   # Create a list of files matching our CPS year.
   files <- list.files(path = "data", pattern = input, full.names = TRUE)
 
@@ -28,20 +27,20 @@ cps_process <- function(year){
     file_name_short <- gsub("data/", "", file_name)
     files[i] <- file_name
     i = i+1}
-  
+
   # Process CPS .dat file into CSV.
 
   if (year < 2014 || year > 2019) {
       print("ERROR:  Please enter a single year (e.g., 2018) between the year range 2014 - 2019.")
-    
+
   } else if (length(files) == 0) {
       print("ERROR:  Data for the given year not found. Download data using the download_cps() function.")
-    
+
   } else {
     for (file_name in files) {
-      
+
       print(paste0('>1 Reading CPS file ', file_name_short, ' into memory.'))
-      
+
       cps_file <- if (year == 2014) {
           read_fwf(file_name, fwf_cols(hrhhid = c(1,15),
                                      hrmonth = c(16,17),
@@ -508,7 +507,7 @@ cps_process <- function(year){
                                      ptst1tn = c(1163,1166),
                                      ptsd18tn = c(1167,1170),
                                      prdispwk = c(1171,1172)))
-        
+
       } else if (year == 2015 || year == 2016) {
           read_fwf(file_name, fwf_cols(hrhhid = c(1,15),
                                        hrmonth = c(16,17),
@@ -1454,13 +1453,13 @@ cps_process <- function(year){
                                      ptsd18tn = c(1217,1220),
                                      prdispwk = c(1221,1222)))
       }
-      
+
       print(paste0('>>2 Determining column widths for ', file_name_short, '.'))
-      
+
       print('>>>3 Assigning variable names to dataframe.')
-      
+
       print(">>>>4 Writing .csv file to working directory.")
-      
+
       write.csv(cps_file, paste0('data/', substr(file_name_short, 1, 8), ".csv"), row.names = FALSE)
     }
   }
